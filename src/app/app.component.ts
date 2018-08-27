c224d6f1303f3b3fd147453421033f9b9492fbc2
Luca Mazzotti <luca.mazzotti@exage.com>
2018-08-27 11:24:20 +0200

    Basic example increment decrement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppState } from "./app-state";
+import { Store } from "@ngrx/store";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  counter$; // variable$ mean that is a observable
+
+  constructor(private store: Store<AppState>){
+  	this.counter$ = store.select("counter");
+  }
+
+  increment() {
+  	this.store.dispatch({ type: "INCREMENT" });
+  }
+
+  decrement() {
+	  this.store.dispatch({ type: "DECREMENT" });
+  }
+
 }
